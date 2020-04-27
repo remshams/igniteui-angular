c9bdc2cc365fd0acd155542af85963de4aaf1314
Nadia Robakova <nrobakova@infragistics.com>
2020-04-27 12:12:53 +0300

    chore(*): add test project

---
+++ projects/igniteui-angular-e2e/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'igniteui-angular-e2e';
+}
