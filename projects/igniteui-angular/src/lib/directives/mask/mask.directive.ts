10aa23710a510266da4101113ae441158a34b24a
jackofdiamond5 <16020256+jackofdiamond5@users.noreply.github.com>
2021-06-18 14:53:24 +0300

    fix(mask): mask changes dynamically

--- projects/igniteui-angular/src/lib/directives/mask/mask.directive.ts
+++ projects/igniteui-angular/src/lib/directives/mask/mask.directive.ts
@@ -29,10 +29,13 @@ export class IgxMaskDirective implements OnInit, AfterViewChecked, ControlValueA
 
     public set mask(val: string) {
         // B.P. 9th June 2021 #7490
+        const oldOptions = this.maskOptions;
         if (val !== this._mask) {
             this.setPlaceholder(val);
             this._mask = val;
         }
+        const cleanInputValue = this.maskParser.parseValueFromMask(this.inputValue, oldOptions);
+        this.updateInputValue(cleanInputValue);
     }
 
     /**
@@ -252,7 +255,7 @@ export class IgxMaskDirective implements OnInit, AfterViewChecked, ControlValueA
             return;
         }
         this._focused = true;
-        this.showMask(this._dataValue);
+        this.showMask(this.inputValue);
     }
 
     /** @hidden */
@@ -335,7 +338,7 @@ export class IgxMaskDirective implements OnInit, AfterViewChecked, ControlValueA
             // TODO(D.P.): focusedValuePipe should be deprecated or force-checked to match mask format
             this.inputValue = this.focusedValuePipe.transform(value);
         } else {
-            this.inputValue = this.maskParser.applyMask(this.inputValue, this.maskOptions);
+            this.inputValue = this.maskParser.applyMask(value, this.maskOptions);
         }
 
         this._oldText = this.inputValue;
@@ -360,6 +363,16 @@ export class IgxMaskDirective implements OnInit, AfterViewChecked, ControlValueA
         const placeholder = this.nativeElement.placeholder;
         if (!placeholder || placeholder === this.mask) {
             this.renderer.setAttribute(this.nativeElement, 'placeholder', value || this.defaultMask);
+        }
+    }
+
+    private updateInputValue(value: string) {
+        if (this._focused) {
+            this.showMask(value);
+        } else if (this.displayValuePipe) {
+            this.inputValue = this.displayValuePipe.transform(value);
+        } else {
+            this.inputValue = this.inputValue ? this.maskParser.applyMask(value, this.maskOptions) : '';
         }
     }
 
